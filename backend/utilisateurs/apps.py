ff426b93af9496b682f715c30f0f241457fcfa40
Yves Mael <yvesmaelt@gmail.com>
2025-06-19 22:13:43 +0100

    Branche ou seront mis les fichiers communs et BD

--- backend/utilisateurs/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class UtilisateursConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'utilisateurs'
-
-    def ready(self):
-        import utilisateurs.signals  # Cette importation permet de declencher le signal 
